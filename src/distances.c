3f64df7af05aed980c5601f4428034ccce0f93cb
Łukasz Lepianka <Lukis30888@gmail.com>
2026-06-13 04:48:19 +0200

    feat: add matrix represenation shift

--- src/distances.c
+++ src/distances.c
@@ -10,3 +10,16 @@ double euclidean_distance(double* a, double* b, size_t n){
     sum = sqrt(sum);
     return sum;
 }
+
+void col_to_row_major(const double *src, double *dst, int nrows, int ncols) {
+    for (int i = 0; i < nrows; i++)
+        for (int j = 0; j < ncols; j++)
+            dst[i * ncols + j] = src[i + j * nrows];
+}
+
+void row_to_col_major(const double *src, double *dst, int nrows, int ncols) {
+    for (int i = 0; i < nrows; i++)
+        for (int j = 0; j < ncols; j++)
+            dst[i + j * nrows] = src[i * ncols + j];
+}
+
